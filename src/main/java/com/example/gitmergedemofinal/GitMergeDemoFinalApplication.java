4583cfdf20900f6b0fcad217253d210a1497f8c3
王钰 <wangyu9@bonc.com.cn>
2022-03-02 19:26:30 +0800

    idea提交远程master

---
+++ src/main/java/com/example/gitmergedemofinal/GitMergeDemoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitmergedemofinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitMergeDemoFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitMergeDemoFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitmergedemofinal/GitMergeDemoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitmergedemofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitMergeDemoFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
